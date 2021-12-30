bf05814f00afab8b357391157fb4926ef9edff69
AnsonRaphael <anson550220@gmail.com>
2021-12-31 01:08:34 +0530

    both send and reply msg

---
+++ src/main/java/com/nanos/nanosjms/listener/HelloMessageListener.java
@@ -0,0 +1,45 @@
+package com.nanos.nanosjms.listener;
+
+import com.nanos.nanosjms.config.JmsConfig;
+import com.nanos.nanosjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class HelloMessageListener {
+    private final JmsTemplate jmsTemplate;
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message){
+
+//        System.out.println("I Got a Message!!!!!");
+//
+//        System.out.println(helloWorldMessage);
+
+    }
+
+    // reply option
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                               @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World!!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+
+    }
+}
