5e0b117a1c43b7565a0ec0cc4e18150cf3fc25a2
AnsonRaphael <anson550220@gmail.com>
2021-12-31 00:13:57 +0530

    initial commit

---
+++ src/main/java/com/nanos/nanosjms/NanosJmsApplication.java
@@ -0,0 +1,13 @@
+package com.nanos.nanosjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanosJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanosJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanos/nanosjms/NanosJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanos.nanosjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanosJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
